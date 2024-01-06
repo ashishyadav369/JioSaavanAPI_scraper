ff0bf5555d91f6badbc013317da9b69323093921
Ashish Yadav <84495458+LeFo-u@users.noreply.github.com>
2024-01-06 15:24:03 +0530

    added handel error when downloading

--- src/download.js
+++ src/download.js
@@ -4,28 +4,35 @@ const axios = require("axios");
 const ProgressBar = require("progress");
 
 const downloadAndWrite = async (url, songName) => {
-  const { data, headers } = await axios({
-    url,
-    method: "GET",
-    responseType: "stream",
-  });
-  const totalLength = headers["content-length"];
+  try {
+    const { data, headers } = await axios({
+      url,
+      method: "GET",
+      responseType: "stream",
+    });
+    const totalLength = headers["content-length"];
 
-  console.log("Starting download");
-  const progressBar = new ProgressBar("-> downloading [:bar] :percent :etas", {
-    width: 40,
-    complete: "=",
-    incomplete: " ",
-    renderThrottle: 1,
-    total: parseInt(totalLength),
-  });
+    console.log("Starting download");
+    const progressBar = new ProgressBar(
+      "-> downloading [:bar] :percent :etas",
+      {
+        width: 40,
+        complete: "=",
+        incomplete: " ",
+        renderThrottle: 1,
+        total: parseInt(totalLength),
+      }
+    );
 
-  const writer = fs.createWriteStream(
-    path.resolve(process.cwd(), "downloads", `${songName}.mp3`)
-  );
+    const writer = fs.createWriteStream(
+      path.resolve(process.cwd(), "downloads", `${songName}.mp3`)
+    );
 
-  data.on("data", (chunk) => progressBar.tick(chunk.length));
-  data.pipe(writer);
+    data.on("data", (chunk) => progressBar.tick(chunk.length));
+    data.pipe(writer);
+  } catch (error) {
+    console.error("Error downloading !!");
+  }
 };
 
 exports.downloadAndWrite = downloadAndWrite;
