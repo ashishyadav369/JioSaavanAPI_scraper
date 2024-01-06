537fa107e213a04264f6c3aae806c077b1cf7bf1
Ashish Yadav <84495458+LeFo-u@users.noreply.github.com>
2024-01-06 15:31:37 +0530

    code refactoring & better error handeling

--- bin/index.js
+++ bin/index.js
@@ -1,30 +1,48 @@
 #! /usr/bin/env node
 const parseUrl = require("../src/script");
 const audio = require("../src/download");
+const urlParser = require("url");
 
 const option = process.argv[2];
 const url = process.argv[3];
 
-switch (option) {
-  case undefined:
+function handelArgument(option, url) {
+  if (option === undefined) {
     console.log("\nWelcome to scrape :)");
     console.log("Type --help for more information\n ");
-    break;
-  case "--help":
+  } else if (option === "--help") {
     console.log("\nOptions:\r");
-    console.log("    -d, --download\t");
-    console.log("\t--help\t\t      " + "Show help");
-    break;
-
-  case "-d":
-    parseUrl
-      .getSongInfo(url)
-      .then((result) => {
-        (async () => {
-          await audio.downloadAndWrite(result.web_cdn, result.song);
-        })();
-      })
-      .catch((error) => console.error(error));
-    break;
-  default:
+    console.log("    -d, --download\tUsage: scrape -d <song_url>");
+    console.log("\t--help\t\t" + "Show help");
+  } else if (option === "-d" || option === "--download") {
+    if (!url) {
+      console.log("No URL Found");
+      process.exit();
+    }
+    const { hostname, path } = urlParser.parse(url);
+    let subPath = path.split("/")[1];
+    if (hostname === "www.jiosaavn.com") {
+      if (subPath !== "song") {
+        console.log(
+          "\nPlease enter single song url. It seems you are trying to download album or playlist\n--help for usage details\n"
+        );
+        process.exit();
+      }
+      parseUrl
+        .getSongInfo(url)
+        .then((result) => {
+          (async () => {
+            await audio.downloadAndWrite(result.web_cdn, result.song);
+          })();
+        })
+        .catch((error) => console.error(error));
+    } else {
+      console.log(
+        "\nPlease enter valid JioSaavan song link\n--help for usage details\n"
+      );
+      process.exit();
+    }
+  }
 }
+
+handelArgument(option, url);
