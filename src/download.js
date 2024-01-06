186e2d0c6763ff2ba536edd681793af1cfa9b218
Ashish Yadav <84495458+LeFo-u@users.noreply.github.com>
2024-01-06 21:43:32 +0530

    customized progress bar & added support for multiple synchronus download

--- src/download.js
+++ src/download.js
@@ -3,7 +3,7 @@ const path = require("path");
 const axios = require("axios");
 const ProgressBar = require("progress");
 
-const downloadAndWrite = async (url, songName) => {
+const downloadAndWrite = async (url, songName, albumName) => {
   try {
     const { data, headers } = await axios({
       url,
@@ -12,13 +12,16 @@ const downloadAndWrite = async (url, songName) => {
     });
     const totalLength = headers["content-length"];
 
-    console.log("Starting download");
+    // colour for progress bar
+    let green = "\u001b[42m \u001b[0m";
+    let red = "\u001b[41m \u001b[0m";
+
     const progressBar = new ProgressBar(
-      "-> downloading [:bar] :percent :etas",
+      `➡️ DOWNLOADING ➡️ ${songName} from ${albumName} ➡️ ➡️ :bar :percent :etas`,
       {
         width: 40,
-        complete: "=",
-        incomplete: " ",
+        complete: green,
+        incomplete: red,
         renderThrottle: 1,
         total: parseInt(totalLength),
       }
@@ -28,8 +31,21 @@ const downloadAndWrite = async (url, songName) => {
       path.resolve(process.cwd(), "downloads", `${songName}.mp3`)
     );
 
-    data.on("data", (chunk) => progressBar.tick(chunk.length));
-    data.pipe(writer);
+    // Return Promise which is resolved when download is complete
+    return new Promise((resolve, reject) => {
+      data.on("data", (chunk) => progressBar.tick(chunk.length));
+      data.pipe(writer);
+
+      writer.on("finish", () => {
+        console.log(`Download complete for ${songName}`);
+        resolve();
+      });
+
+      writer.on("error", (error) => {
+        console.error(`Error writing file for ${songName}: ${error.message}`);
+        reject(error);
+      });
+    });
   } catch (error) {
     console.error("Error downloading !!");
   }
